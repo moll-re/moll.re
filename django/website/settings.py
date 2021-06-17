616a9380066e872b1636a6c0b64b12188ff7282c
Rémy Moll <36770313+moll-re@users.noreply.github.com>
2021-06-17 19:14:21 +0200

    Update settings.py

--- django/website/settings.py
+++ django/website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^qli=3k7fyn(#f3%=6!czk8a_dlay))41uqr5wvnt16a6+uzeo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
