eb61fefd1716a0305ae7d2fff83b262a85c7276c
Remy Moll <mollator2@gmail.com>
2021-06-30 19:44:05 +0200

    more analytics, better static serving

--- django/analytics/urls.py
+++ django/analytics/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('fetch_total_views', views.fetch_total_views, name='fetch_total_views'),
     path('fetch_relative_views', views.fetch_relative_views, name='fetch_relative_views'),
     path('fetch_aio_sensors', views.fetch_aio_sensors, name='fetch_aio_sensors'),
+    path('fetch_aio_bot_stats', views.fetch_aio_bot_stats, name='fetch_aio_bot_stats'),
+    path('fetch_aio_lists', views.fetch_aio_lists, name='fetch_aio_lists'),
 ]
