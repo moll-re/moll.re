701088e9f93ac4a15c0991a912ecbec80cb497e8
Remy Moll <remoll@ethz.ch>
2021-08-22 16:46:11 +0200

    better lists, cleaner stats

--- django/analytics/urls.py
+++ django/analytics/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('fetch_relative_views', views.fetch_relative_views, name='fetch_relative_views'),
     path('fetch_aio_sensors', views.fetch_aio_sensors, name='fetch_aio_sensors'),
     path('fetch_aio_bot_stats', views.fetch_aio_bot_stats, name='fetch_aio_bot_stats'),
-    path('fetch_aio_lists', views.fetch_aio_lists, name='fetch_aio_lists'),
+    path('fetch_aio_lists/<id>', views.fetch_aio_lists, name='fetch_aio_lists'),
 ]
